6590ac13bda6ae2f4cd599429be5937753a4f3d0
zhaoyangkun <2295098032@qq.com>
2022-01-22 15:42:19 +0800

    优化文章和标签的SQL

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ echo "########## start build aries ##########"
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o aries ./main.go
 # 封装 docker 镜像
 echo "########## start build docker image ##########"
-docker build -t zhaoyangkun/aries .
+docker rmi zhaoyangkun/aries:latest
+docker build -t zhaoyangkun/aries:latest .
 # 推送 docker 镜像
 echo "########## start push docker image ##########"
 docker push zhaoyangkun/aries:latest
