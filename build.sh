6f53ff981987e95a56346930fcd1b6ee5f345c57
zhaoyangkun <1040970792@qq.com>
2022-05-23 13:58:57 +0800

    修改build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o aries ./main.go
 echo "########## start build docker image ##########"
 docker rmi zhaoyangkun/aries:latest
 docker build -t zhaoyangkun/aries:latest .
+docker tag zhaoyangkun/aries:latest registry.cn-hangzhou.aliyuncs.com/zhaoyangkun/aries:latest
 # 推送 docker 镜像
 echo "########## start push docker image ##########"
-docker push zhaoyangkun/aries:latest
+#docker push zhaoyangkun/aries:latest
+docker push registry.cn-hangzhou.aliyuncs.com/zhaoyangkun/aries:latest
 echo "########## all jobs are done ##########"
