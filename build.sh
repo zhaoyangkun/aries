ddababf173035387bcb1f2cc866bcdcb18ad4f3f
zhaoyangkun <10409709792@qq.com>
2022-07-27 12:44:43 +0800

    更新vditor

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o aries ./main.go
 # 封装 docker 镜像
 echo "########## start build docker image ##########"
 docker rmi zhaoyangkun/aries:latest
+docker rmi registry.cn-hangzhou.aliyuncs.com/zhaoyangkun/aries:latest
 docker build -t zhaoyangkun/aries:latest .
 docker tag zhaoyangkun/aries:latest registry.cn-hangzhou.aliyuncs.com/zhaoyangkun/aries:latest
 # 推送 docker 镜像
