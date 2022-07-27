ba1a9dc89db2d82bdab944638a16f19d0dee2e44
zhaoyangkun <10409709792@qq.com>
2022-07-27 11:25:26 +0800

    修改cdn为sourcegcdn

--- config/setting/setting.go
+++ config/setting/setting.go
@@ -145,7 +145,7 @@ func (b *BlogVariable) InitBlogVars(siteSetting map[string]string, socialInfo ma
 		if Config.Server.Mode == gin.DebugMode {
 			b.StaticRootPath = b.ContextPath
 		} else {
-			b.StaticRootPath = "https://gh.sourcegcdn.com/zhaoyangkun/aries/master"
+			b.StaticRootPath = "https://gh.sourcegcdn.com/zhaoyangkun/aries/v1.1.7"
 		}
 	}
 	if siteName, ok := siteSetting["site_name"]; ok {
