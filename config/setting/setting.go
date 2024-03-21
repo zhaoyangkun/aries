5529f2e9c5baacd1f527abad8ca49827eeb9ea84
zhaoyangkun <1040970792@qq.com>
2024-03-21 21:15:42 +0800

    将图片上传方式修改为前端直传（除腾讯云COS外）

--- config/setting/setting.go
+++ config/setting/setting.go
@@ -144,7 +144,7 @@ func (b *BlogVariable) InitBlogVars(siteSetting map[string]string, socialInfo ma
 		if Config.Server.Mode == gin.DebugMode {
 			b.StaticRootPath = b.ContextPath
 		} else {
-			b.StaticRootPath = "https://jsd.onmicrosoft.cn/gh/zhaoyangkun/aries@latest"
+			b.StaticRootPath = "https://jsd.onmicrosoft.cn/gh/zhaoyangkun/aries@1.3.6"
 		}
 	}
 	if siteName, ok := siteSetting["site_name"]; ok {
